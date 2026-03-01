f6885dbfb96fbbbfad6685e0dc6592712bb5d7be
nambatu <julianlangschwert@gmail.com>
2026-03-01 16:24:14 +0100

    ram usage optimization

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true, 
-        args: ['--no-sandbox', '--disable-setuid-sandbox'], 
-        executablePath: '/usr/bin/chromium' 
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],        
+    executablePath: '/usr/bin/chromium' 
     }
 });
 
